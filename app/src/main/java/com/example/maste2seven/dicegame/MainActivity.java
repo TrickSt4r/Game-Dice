7f1dd6b2574016d9cc8d2041706c6edd5e25e429
7six <maste2seven@gmail.com>
2016-02-24 11:51:21 +0700

    Chang View

--- app/src/main/java/com/example/maste2seven/dicegame/MainActivity.java
+++ app/src/main/java/com/example/maste2seven/dicegame/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.maste2seven.dicegame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListPopupWindow;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +14,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     private ImageView image1;
     private Button button,button2, button3;
+    private int val;
 
 
 
@@ -29,18 +31,45 @@ public class MainActivity extends AppCompatActivity {
         Forwarding();
         Backing();
         random();
+        changdice();
 
 
     }//method
+
+    private void changdice() {
+
+        Log.d("dice", "ค่าที่ได้" + val);
+
+        if (val == 1) {
+            image1.setImageResource(R.drawable.dice1);
+        } else if (val == 2) {
+            image1.setImageResource(R.drawable.dice2);
+        } else if (val == 3) {
+            image1.setImageResource(R.drawable.dice3);
+        } else if (val == 4) {
+            image1.setImageResource(R.drawable.dice4);
+        } else if (val == 5) {
+            image1.setImageResource(R.drawable.dice5);
+        } else if (val == 6) {
+            image1.setImageResource(R.drawable.dice6);
+        }
+
+    }
 
     private void random() {
 
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Random objRandom = new Random();
+                val = objRandom.nextInt(6) + 1;
+
+                changdice();
+                }
+
 
             }
-        });
+        );
 
     }//randomroll
 
@@ -48,7 +77,11 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                val = val-1;
+                if (val==1) {
+                    val = 6;
+                }
+                changdice();
             }
         });
 
@@ -60,10 +93,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-            }
-        });
+                val = val+1;
+                if (val == 7) {
+                    val = 1;
+                }
 
-    }
+                changdice();
+            }
+                    }
+
+        );
+
+}
 
 
 
