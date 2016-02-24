f38757185404df4abe362a1957c9a2805b69ce33
7six <maste2seven@gmail.com>
2016-02-24 10:42:50 +0700

    bindwiget Succsucefully

--- app/src/main/java/com/example/maste2seven/dicegame/MainActivity.java
+++ app/src/main/java/com/example/maste2seven/dicegame/MainActivity.java
@@ -2,18 +2,60 @@ package com.example.maste2seven.dicegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListPopupWindow;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView image1;
+    private Button button,button2, button3;
 
-    public void rolldice(){
 
 
 
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image1 = (ImageView) findViewById(R.id.imageView);
+
+        bindwiget();
+        Forwarding();
+        Backing();
+        random();
+
+
     }
+
+    private void random() {
+
+    }
+
+    private void Backing() {
+
+
+    }
+
+    private void Forwarding() {
+
+
+    }
+
+    private void bindwiget() {
+
+        image1 = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+
+    }
+
+
 }
